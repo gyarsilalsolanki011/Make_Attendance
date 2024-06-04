47e38079ae8559009eb2358dab5f50bde9363a9d
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-06-04 14:09:36 +0530

    Student fragments added + List Fragment

--- app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/adapter/AttendanceViewRecyclerAdapter.java
+++ app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/adapter/AttendanceViewRecyclerAdapter.java
@@ -1,12 +1,16 @@
 package com.gyarsilalsolanki011.makeattendance.activities.student.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gyarsilalsolanki011.makeattendance.R;
 import com.gyarsilalsolanki011.makeattendance.activities.student.model.AttendanceViewModel;
 
 import java.util.ArrayList;
@@ -23,12 +27,19 @@ public class AttendanceViewRecyclerAdapter extends RecyclerView.Adapter<Attendan
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.attendance_view, parent, false);
+        return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull AttendanceViewRecyclerAdapter.ViewHolder holder, int position) {
 
+        AttendanceViewModel model = (AttendanceViewModel) arrayListAttendance.get(position);
+        holder.subject.setText(model.getSubject());
+        holder.notification.setText(model.getNotification());
+        holder.classAttended.setText(model.getClassesAttended().toString());
+        holder.classConducted.setText(model.getClassesConducted().toString());
     }
 
     @Override
@@ -37,8 +48,13 @@ public class AttendanceViewRecyclerAdapter extends RecyclerView.Adapter<Attendan
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView subject, notification, classAttended, classConducted;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            subject = itemView.findViewById(R.id.subjectTextView);
+            notification = itemView.findViewById(R.id.shortListNotification);
+            classAttended = itemView.findViewById(R.id.classes_attended);
+            classConducted = itemView.findViewById(R.id.classes_conducted);
         }
     }
 }
--- app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/fragments/StudentHomeFragment.java
+++ app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/fragments/StudentHomeFragment.java
@@ -1,17 +1,30 @@
 package com.gyarsilalsolanki011.makeattendance.activities.student.fragments;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gyarsilalsolanki011.makeattendance.R;
+import com.gyarsilalsolanki011.makeattendance.activities.student.adapter.AttendanceViewRecyclerAdapter;
+import com.gyarsilalsolanki011.makeattendance.activities.student.model.AttendanceViewModel;
+
+import java.util.ArrayList;
 
 
 public class StudentHomeFragment extends Fragment {
+    ArrayList<AttendanceViewModel> arrayListAttendance;
+    private ProgressDialog progressDialog;
+    AttendanceViewRecyclerAdapter adapter;
 
     public StudentHomeFragment() {
         // Required empty public constructor
@@ -22,5 +35,57 @@ public class StudentHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        openProgressDialog();
+        dataInitialize();
+
+        RecyclerView recyclerView = view.findViewById(R.id.viewRecycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new AttendanceViewRecyclerAdapter(getContext(), arrayListAttendance);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        if (progressDialog.isShowing()) progressDialog.dismiss();
+    }
+
+    private void dataInitialize() {
+
+        arrayListAttendance = new ArrayList<>();
+
+        String[] subjectList = new String[]{
+                getString(R.string.maths),
+                getString(R.string.maths),
+                getString(R.string.maths),
+                getString(R.string.maths),
+                getString(R.string.maths),
+        };
+
+        String[] notificationList = new String[]{
+                getString(R.string.notification1),
+                getString(R.string.notification2),
+                getString(R.string.notification1),
+                getString(R.string.notification2),
+                getString(R.string.notification1)
+        };
+
+        for (int i=0; i<subjectList.length; i++){
+            AttendanceViewModel model = new AttendanceViewModel(subjectList[i], notificationList[i], 2, 2);
+            arrayListAttendance.add(model);
+        }
+
+    }
+
+    private void openProgressDialog() {
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setCancelable(true);
+        progressDialog.setMessage("Fetching Data....");
+        progressDialog.show();
+
     }
 }
