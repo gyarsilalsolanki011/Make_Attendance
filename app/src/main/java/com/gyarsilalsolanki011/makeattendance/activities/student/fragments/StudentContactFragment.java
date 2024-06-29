c74cd26dc9700ea558a893ff62fbd0f5bb180735
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-06-29 07:39:12 +0530

    just Modified

--- app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/fragments/StudentContactFragment.java
+++ app/src/main/java/com/gyarsilalsolanki011/makeattendance/activities/student/fragments/StudentContactFragment.java
@@ -3,20 +3,21 @@ package com.gyarsilalsolanki011.makeattendance.activities.student.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
 import com.gyarsilalsolanki011.makeattendance.R;
 
 public class StudentContactFragment extends Fragment {
-    Button btnEmail, btnMsg, btnShare;
+    TextView whatsapp,feedback,send_your_message;
+    ImageView linkedin, instagram, github, share;
     public StudentContactFragment() {
         // Required empty public constructor
     }
@@ -29,33 +30,103 @@ public class StudentContactFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        btnEmail = view.findViewById(R.id.btnEmail);
-        btnMsg = view.findViewById(R.id.btnMsg);
-        btnShare = view.findViewById(R.id.btnShare);
-
-        btnMsg.setOnClickListener(v -> {
-            Intent iMsg = new Intent(Intent.ACTION_SENDTO);
-            iMsg.setData(Uri.parse("smsto:"+Uri.encode("+917620824421")));
-            iMsg.putExtra("sms_body", "Please solve this asap.");
-            startActivity(iMsg);
+    public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+        whatsapp = v.findViewById(R.id.whatsapp);
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uriText = "https://api.whatsapp.com/send/"+"?phone="+"7909519946"+"&text="+
+                        "Please+help+me+regarding+the+issue+:+"+"&type=phone_number&app_absent=0";
+                gotoUrl(uriText);
+            }
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
         });
 
-        btnEmail.setOnClickListener(v -> {
-            Intent iEmail = new Intent(Intent.ACTION_SEND);
-            iEmail.setType("message/rfc922");
-            iEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{"gyarsilals105@gmail.com"});
-            iEmail.putExtra(Intent.EXTRA_SUBJECT, "Regarding _ Issue");
-            iEmail.putExtra(Intent.EXTRA_TEXT,"Please resolve this issue asap.");
-            startActivity(Intent.createChooser(iEmail, "Email via"));
+
+        feedback = v.findViewById(R.id.feedback);
+        feedback.setOnClickListener(view -> {
+
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            String uriText = "mailto:" + Uri.encode("gyarsilalsolanki011@gmail.com") + "?subject=" +
+                    Uri.encode("Enter Your email") + "&body=" + Uri.encode("");
+
+            Uri uri = Uri.parse(uriText);
+            intent.setData(uri);
+            startActivity(Intent.createChooser(intent, "Send Email"));
+
         });
 
-        btnShare.setOnClickListener(v -> {
-            Intent iShare =  new Intent(Intent.ACTION_SEND);
-            iShare.setType("text/plain");
-            iShare.putExtra(Intent.EXTRA_TEXT, "This app is available on Github repo, please visit and check it out, https://github.com/gyarsilalsolanki011/Make_Attendance.git");
-            startActivity(Intent.createChooser(iShare,"Share via"));
+
+
+        send_your_message = v.findViewById(R.id.send_your_message);
+        send_your_message.setOnClickListener(v1 -> {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            String uriText = "mailto:" + Uri.encode("gyarsials105@gmail.com") + "?subject=" +
+                    Uri.encode("Ask yor query") + "&body=" + Uri.encode("please make sure to write name and phone number too");
+
+            Uri uri = Uri.parse(uriText);
+            intent.setData(uri);
+            startActivity(Intent.createChooser(intent, "Send Email"));
+        });
+
+
+        linkedin = v.findViewById(R.id.linkedin);
+        linkedin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String uriText = "https://www.linkedin.com/in/gyarsilalsolanki";
+                gotoUrl(uriText);
+            }
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+
+        github = v.findViewById(R.id.github);
+        github.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String uriText = "https://github.com/gyarsilalsolanki011";
+                gotoUrl(uriText);
+            }
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+
+
+        instagram = v.findViewById(R.id.instagram);
+        instagram.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String uriText = "https://www.instagram.com/itz_gsl_tiger?igsh=czZoZmo4emZuYjIz";
+                gotoUrl(uriText);
+            }
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+
+
+
+        share = v.findViewById(R.id.share);
+        share.setOnClickListener(view -> {
+
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            String shareBody = "look all Programmings";
+            String subject = "https://github.com/gyarsilalsolanki011?tab=repositories";
+            i.putExtra(Intent.EXTRA_SUBJECT,shareBody);
+            i.putExtra(Intent.EXTRA_TEXT,subject);
+            startActivity(Intent.createChooser(i,"Share via"));
+
         });
     }
 }
